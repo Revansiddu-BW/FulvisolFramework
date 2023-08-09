1d8b8a79382641e4b2672ba150c55918b6aace89
hp <hp@DESKTOP-F0LG1DD>
2023-08-09 12:06:22 +0530

    First Commit

---
+++ Fulvisol/src/test/java/com/sales/procrumentTest/createRequestion.java
@@ -0,0 +1,8 @@
+package com.sales.procrumentTest;
+
+public class createRequestion {
+    
+	public static void main(String[] args) {
+		System.out.println("git");
+	}
+}
---
+++ Fulvisol/src/test/java/com/sales/procrumentTest/nonCatlogIteaTest.java
@@ -0,0 +1,8 @@
+package com.sales.procrumentTest;
+
+public class nonCatlogIteaTest {
+    public static void main(String[] args) {
+		
+    	System.out.println("github");
+	}
+}
